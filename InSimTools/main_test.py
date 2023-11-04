a6da01954bf4a835221ffc4e3a2f8fd13229bbc3
Kristof S <49072161+Kramer84@users.noreply.github.com>
2023-11-04 02:05:12 +0100

    Update main_test.py

--- InSimTools/main_test.py
+++ InSimTools/main_test.py
@@ -5,7 +5,7 @@ import InSimRequests as isr
 import Enums as enums
 
 if __name__ == "__main__":
-	insim = pyinsim.insim(host='188.122.74.155', port=55942, Admin='Budapest123', Interval=500)
+	insim = pyinsim.insim(host='188.122.74.155', port=55942, Admin='suKR45_eff', Interval=500)
 	LiveServerState = ldm.LiveServerState(insim)
 	ServerGeneralEventHandler = ish.ServerGeneralEventHandler(insim, LiveServerState, True)
 	pyinsim.run()
